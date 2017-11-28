e021ec8ca3a2cca47c2b3fae6b3d811373831ad4
desile <pl9lar@gmail.com>
2017-11-29 00:31:27 +0300

    returning to first solution

--- make_repo.sh
+++ make_repo.sh
@@ -6,15 +6,15 @@ git -C repo commit -m "init [init.png] => logo.data"
 git -C repo branch -m init
 
 git -C repo checkout -b bugfix
-python -c "import hhgit; hhgit.to_text(['img/init.png', 'img/bugfix.png',], 'repo/logo.data')"
+python -c "import hhgit; hhgit.to_text(['img/bugfix.png', 'img/init.png',], 'repo/logo.data')"
 git -C repo add logo.data
-git -C repo commit -m "bugfix [init.png bugfix.png] => logo.data"
+git -C repo commit -m "bugfix [bugfix.png init.png] => logo.data"
 
 git -C repo checkout init
 git -C repo checkout -b feature1
-python -c "import hhgit; hhgit.to_text(['img/init.png', 'img/refactoring.png'], 'repo/logo.data')"
+python -c "import hhgit; hhgit.to_text(['img/refactoring.png', 'img/init.png'], 'repo/logo.data')"
 git -C repo add logo.data
-git -C repo commit -m "feature1 [init.png refactoring.png] => logo.data"
+git -C repo commit -m "feature1 [refactoring.png init] => logo.data"
 
 git -C repo checkout init
 git -C repo checkout -b rc
@@ -25,9 +25,9 @@ git -C repo merge --commit -m "bugfix to rc" bugfix
 
 git -C repo checkout feature1
 git -C repo checkout -b feature2
-python -c "import hhgit; hhgit.to_text(['img/feature2.png', 'img/init.png', 'img/refactoring.png'], 'repo/logo.data')"
+python -c "import hhgit; hhgit.to_text(['img/refactoring.png','img/init.png', 'img/feature2.png'], 'repo/logo.data')"
 git -C repo add logo.data
-git -C repo commit -m "feature2 [feature2.png init.png refactoring.png] => logo.data"
+git -C repo commit -m "feature2 [refactoring.png init feature2.png] => logo.data"
 
 git -C repo checkout feature1
 python -c "import hhgit; hhgit.to_text(['img/init.png'], 'repo/logo.data')"
