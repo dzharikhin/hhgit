9a5d6c56973bc05ba63d5b202cc51e809d5d87d6
S.V. Velichko <msiostream@gmail.com>
2017-12-03 00:12:28 +0300

    Get rid of any conflicts on mergers
    bugfix had to be done in alternative way to avoid conflicts when trying
    to merge feature2 with rc. feature1 (without refactoring) cannot be
    merged without conflicts in this case (gray arrow in the structure cannot
    be recreated).

--- hw.sh
+++ hw.sh
@@ -24,10 +24,10 @@ fi
 # Bugfix
 git checkout -b bugfix
 cd ..
-python -c "import hhgit; hhgit.to_text(['img/init.png', 'img/bugfix.png'], '$TREPO/main.data')"
+python -c "import hhgit; hhgit.to_text(['img/bugfix.png', 'img/init.png'], '$TREPO/main.data')"
 cd $TREPO
 git add main.data
-git commit -m "bugfix [init.png bugfix.png] -> main.data"
+git commit -m "bugfix [bugfix.png init.png] -> main.data"
 if [ ! -z $DEBUG ]
 then
 	cd ..
@@ -61,18 +61,7 @@ then
 	cd $TREPO
 fi
 
-Merge Feature 1 into RC
-git checkout rc
-git merge feature1 -m "merge feature1 into rc"
-if [ ! -z $DEBUG ]
-then
-	cd ..
-	python -c "import hhgit; hhgit.from_text(['$TREPO/main.data'], '$DEBUG/rc2.png')"
-	cd $TREPO
-fi
-
 # Feature 1 - Refactoring
-git checkout feature1
 git checkout -b feature1-refactoring
 cd ..
 python -c "import hhgit; hhgit.to_text(['img/refactoring.png'], '$TREPO/background.data')"
@@ -117,12 +106,6 @@ fi
 # Merge Feature 2 into RC
 git checkout rc
 git merge feature2 -m "merge feature2 into rc"
-grep -v '<<<' < main.data > main1.data
-grep -v '===' < main1.data > main2.data
-grep -v '>>>' < main2.data > main.data
-rm main1.data main2.data
-git add main.data
-git commit -m "merge feature2 into rc"
 if [ ! -z $DEBUG ]
 then
 	cd ..
