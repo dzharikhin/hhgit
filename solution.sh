c72deb1b2b1d1ea41483984e5c5d9bf4ee43e26a
Vladimir Maximov <vmakxs@gmail.com>
2017-12-04 17:36:19 +0300

    Fixed

--- solution.sh
+++ solution.sh
@@ -8,41 +8,41 @@ git init $REPO
 python -c "import hhgit; hhgit.to_text(['img/init.png'], '$REPO/logo.data')"
 cd $REPO
 git add logo.data
-git commit -m "init"
+git commit -m "{init} [init.png] -> {logo.data}"
 
 git checkout -b bugfix
 cd ..
 python -c "import hhgit; hhgit.to_text(['img/bugfix.png', 'img/init.png'], '$REPO/logo.data')"
 cd $REPO
-git commit -am "bugfix"
+git commit -am "{bugfix} [bugfix.png init.png] -> {logo.data}"
 
 git checkout master
 git checkout -b feature1
 cd ..
 python -c "import hhgit; hhgit.to_text(['img/refactoring.png', 'img/init.png'], '$REPO/logo.data')"
 cd $REPO
-git commit -am "feature1"
+git commit -am "{feature1} [refactoring.png img/init.png] -> {logo.data}"
 
 git checkout -b feature2
 cd ..
 python -c "import hhgit; hhgit.to_text(['img/refactoring.png', 'img/init.png', 'img/feature2.png'], '$REPO/logo.data')"
 cd $REPO
-git commit -am "feature2"
+git commit -am "{feature2} [refactoring.png', init.png', feature2.png] -> {logo.data}"
 
 git checkout feature1
-git checkout -b refactoring
+git checkout -b feature1-refactoring
 cd ..
 python -c "import hhgit; hhgit.to_text(['img/init.png'], '$REPO/logo.data')"
 python -c "import hhgit; hhgit.to_text(['img/refactoring.png'], '$REPO/back.data')"
 cd $REPO
 git add --all
-git commit -am "feature2"
+git commit -am "{feature1-refactoring} [init.png] -> {logo.data}, [refactoring.png] -> {back.data}"
 
 git checkout master
 git checkout -b rc
-git merge bugfix -m "rc0" --no-ff
-git merge refactoring -m "rc1" --no-ff
-git merge feature2 -m "rc2" --no-ff
+git merge bugfix -m "{rc}" --no-ff
+git merge feature1-refactoring -m "{rc}" --no-ff
+git merge feature2 -m "{rc}" --no-ff
 
 git checkout master
 git merge rc
@@ -52,7 +52,7 @@ python -c "import hhgit; hhgit.from_text(['$REPO/logo.data'], 'logo.png')"
 python -c "import hhgit; hhgit.from_text(['$REPO/back.data'], 'back.png')"
 
 cd $REPO
-git filter-branch -f --tree-filter "find * -name '*.data' -type f -exec sed -i '1i\4 hours' {} +" HEAD --all
+git filter-branch -f --tree-filter "find * -name '*.data' -type f -exec sed -i '1i\#4 hours' {} +" HEAD --all
 cd ..
 
 echo "All done."
