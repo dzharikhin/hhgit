d9927ce57cdaa1a2409ed05c50a27cf577814c36
i.sannikov <i.sannikov@sirena2000.ru>
2017-12-11 12:13:25 +0300

    fix master merge

--- make_repo.sh
+++ make_repo.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -e train_repo ] ; then
+  rm -rf train_repo
+fi
+
 mkdir train_repo
 
 #init
@@ -12,10 +16,10 @@ git commit -m "init [init.png]->main.data"
 # bugfix
 git checkout -b bugfix
 cd ..
-python -c "import hhgit; hhgit.to_text(['img/init.png', 'img/bugfix.png'], 'train_repo/main.data')"
+python -c "import hhgit; hhgit.to_text(['img/bugfix.png', 'img/init.png'], 'train_repo/main.data')"
 cd train_repo
 git add main.data
-git commit -m "bugfix [init.png, bugfix.png]->main.data"
+git commit -m "bugfix [bugfix.png, init.png]->main.data"
 
 #feature1
 git checkout master
@@ -39,29 +43,21 @@ git commit -m "refactoring [init.png]->main.data, [refactoring.png]->ground.data
 git checkout feature1
 git checkout -b feature2
 cd ..
-python -c "import hhgit; hhgit.to_text(['img/refactoring.png', 'img/feature2.png', 'img/init.png'], 'train_repo/main.data')"
+python -c "import hhgit; hhgit.to_text(['img/refactoring.png', 'img/init.png', 'img/feature2.png'], 'train_repo/main.data')"
 cd train_repo
 git add main.data
-git commit -m "feature1 [refactoring.png, feature2.png, init.png]->main.data"
+git commit -m "feature2 [refactoring.png, init.png, feature2.png]->main.data"
 
 #rc first step
 git checkout master
 git checkout -b rc
-git merge bugfix --no-ff -m "rc [init.png, bugfix.png]->main.data"
+git merge bugfix --no-ff -m "rc [bugfix.png, init.png]->main.data"
 
 #rc second step
-git merge feature1 -m "rc [refactoring.png, init.png, bugfix.png]->main.data"
+git merge refactoring -m "rc [bugfix.png, init.png]->main.data, [refactoring.png]->ground.data"
 
 #rc third step
-git merge refactoring -m "rc [init.png, bugfix.png]->main.data, [refactoring.png]->ground.data"
-
-#rc fourth step
-git merge feature2
-cd ..
-python -c "import hhgit; hhgit.to_text(['img/feature2.png', 'img/init.png', 'img/bugfix.png'], 'train_repo/main.data')"
-cd train_repo
-git add main.data
-git commit -m "rc [feature2.png, init.png, bugfix.png]->main.data, [refactoring.png]->ground.data"
+git merge feature2 -m "rc [bugfix.png, init.png, feature2.png]->main.data, [refactoring.png]->ground.data"
 
 #advanced
-git filter-branch --tree-filter 'for filename in * ; do sed -i -e "1 s/^/# 2.5 hours\n/;" $filename ; done' HEAD
+git filter-branch --tree-filter 'for filename in * ; do sed -i -e "1 s/^/# 3 hours\n/;" $filename ; done' HEAD
